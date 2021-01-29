0efb9f27c19dde201339a5a0047c511010db53f2
Andres Perez <aperez@salesforce.com>
2021-01-29 06:05:41 -0500

    Step 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
@@ -8,6 +9,6 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://{HOST}:${port}`)
 })
